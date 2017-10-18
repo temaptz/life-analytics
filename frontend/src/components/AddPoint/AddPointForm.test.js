75fceae142901e62b2bc2aedb7ad56b60596d026
temaptz <temaptz@gmail.com>
2017-10-18 16:48:32 +0300

    Автоматическая конфигурация для окружений

---
+++ frontend/src/components/AddPoint/AddPointForm.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import AddPointForm from './AddPointForm';
+
+it('AddPointForm renders', () => {
+    shallow(<AddPointForm />);
+});
--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,2 +1,11 @@
-export const apiUrl = '/api';
+const localApiUrl      = 'http://localhost:8000';
+const productionApiUrl = '/api';
+
+let currentApiUrl = productionApiUrl;
+
+if ( process.env.NODE_ENV === 'development' ) {
+    currentApiUrl = localApiUrl;
+}
+
+export const apiUrl         = currentApiUrl;
 export const googleClientId = '950154841564-2sv9i8qvhpriadqckbvreg3ki93g15h9.apps.googleusercontent.com';
